ce5245510f808c73a47065059172228b43440d20
metaIhead <vipkingston1000@mail.ru>
2019-05-16 09:28:24 +0800

    15.05.2015 lap n-net,skeleton

---
+++ n-net/sk.py
@@ -0,0 +1,25 @@
+class neuralNetwork:
+
+    def _init_(self,inputnodes, hiddennodes,outputnodes,learningrate):
+        self.inodes=inputnodes
+        self.hnodes=hiddennodes
+        self.onodes=learningrate
+
+        self.lr=learningrate
+        pass
+
+    def train():
+        pass
+
+    def query():
+        pass
+
+
+input_nodes=3
+output_nodes=3
+hidden_nodes=3
+
+
+learning_rate=0.3
+
+n=neuralNetwork(input_nodes,output_nodes,hidden_nodes,hidden_nodes)
