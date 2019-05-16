1a7c2ea47cc3890b6511d0ac6642ba7c1eef9ec0
metaIhead <vipkingston1000@mail.ru>
2019-05-16 16:13:31 +0800

    data collector FTP 16.05.2019 lap

--- data_collector.py
+++ data_collector.py
@@ -1,16 +1,34 @@
+from time import sleep
 from ftplib import FTP, error_perm
 
-def walk(cd='/'):
-    ftp.cwd(cd)
-    print(cd)
-    print(ftp.retrlines('LIST'))
-    for i in ftp.nlst():
-        try:
-            walk(cd + i + '/')
-        except error_perm:
-            print(i)
+ftp = FTP("ssw.iszf.irk.ru", "anonymous", "anonymous")
 
-ftp = FTP('mirror.yandex.ru')
-ftp.login()
-walk()
-ftp.quit()
+ftp.cwd("RAO/srh48/2017/")
+months=ftp.nlst()
+
+for month in months:
+    ftp.cwd(month)
+    days=ftp.nlst()
+    for day in days:
+        print("month: "+month+" -- "+"day: "+day)
+        ftp.cwd(day)
+        fits=ftp.nlst()
+        print(ftp.pwd())
+        for fit in fits:
+            path=str('ftp://ssw.iszf.irk.ru/RAO/srh48/2017/01/01/mf_20170101_020541.fit')
+            #print(path)
+
+
+            # handle = open(path.rstrip("/") + "/" + fit.lstrip("/"), 'wb')
+            # ftp.retrbinary('RETR %s' % fit, handle.write)
+            #path="/home/ivan/"
+            ftp.retrbinary("STOR " + fit ,open(path, 'rb').write)
+
+            print("loaded")
+            ftp.cwd("..")
+            sleep(5)
+
+
+
+    # for day in list(range(1.2)):
+    #     pass
