7511bddbad9240f35b0c64094e3ddad6dbb60e56
metaIhead <vipkingston1000@mail.ru>
2019-05-17 10:52:28 +0800

    17.05.2019 опрос сети

--- n-net/sk.py
+++ n-net/sk.py
@@ -1,25 +1,46 @@
+import numpy as np
+import scipy.special as sci
+
 class neuralNetwork:
 
-    def _init_(self,inputnodes, hiddennodes,outputnodes,learningrate):
+    def __init__(self,inputnodes,hiddennodes,outputnodes,learningrate):
         self.inodes=inputnodes
         self.hnodes=hiddennodes
-        self.onodes=learningrate
+        self.onodes=outputnodes
+
+        #матрицы весовых коэфицентов
+        self.wih = np.random.normal(0.0, pow(self.hnodes, -0.5),(self.hnodes, self.inodes))
+        self.who = np.random.normal(0.0, pow(self.onodes, -0.5),(self.onodes, self.hnodes))
 
         self.lr=learningrate
-        pass
+
+        #функция актифации - сигмойда
+        self.activation_func= lambda x: sci.expit(x)
+
 
     def train():
         pass
 
-    def query():
-        pass
+        
+    #опрос сети
+    def query(self, inputs_list):
+        #список входных значений в в двуменрый масив
+        inputs=np.array(inputs_list, ndim=2).T
 
+        #расчитываем входящие сигналы для скрытого слоя
+        hidden_inputs=numpy.dot(self.whi, inputs)
+        #исходящие сигналы для скрытого слоя
+        hidden_outputs=self.activation_func(hidden_inputs)
+
+        #входящие сигналя для выходного слоя
+        final_inputs=np.dot(self.who, hidden_outputs)
+        #исходящие синалы для выходного слоя
+        final_outputs=self.activation_func(final_inputs)
 
 input_nodes=3
 output_nodes=3
 hidden_nodes=3
 
-
 learning_rate=0.3
 
-n=neuralNetwork(input_nodes,output_nodes,hidden_nodes,hidden_nodes)
+n=neuralNetwork(input_nodes,output_nodes,hidden_nodes,learning_rate)
